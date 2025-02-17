60a9f5758259b235aeb862a41fa38470c05234d8
Tom Moor <tom@getoutline.com>
2025-02-16 20:10:18 -0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,6 @@
-// utils.js
 function getBaseUrl(bundle) {
-    // Use the user-supplied URL or fallback to the default.
-    return bundle.authData.instance_url || 'https://app.getoutline.com';
+  // Use the user-supplied URL or fallback to the default.
+  return bundle.authData.instance_url || 'https://app.getoutline.com';
 }
 
 module.exports = { getBaseUrl };
